4bfaedb6fc6da839410d6976c3ea1a5321b98261
joppepoppe <jonatan.holmgren@hotmail.com>
2016-01-21 14:41:47 +0100

    Img

--- Assignment_1/src/se/mah/ke/k3lara/Main.java
+++ Assignment_1/src/se/mah/ke/k3lara/Main.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
 
 public class Main extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +40,43 @@ public class Main extends JFrame {
 	 */
 	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 508, 520);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 116, 460);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"\u00C5ke"},
+				{"Jakob"},
+				{"Erik"},
+				{"Matilda"},
+				{"Lorem"},
+				{"Ipsum"},
+				{"Lucas"},
+				{"Madde"},
+				{"Anita"},
+				{"Clas"},
+				{"Oskar"},
+				{"Josefine"},
+				{null},
+			},
+			new String[] {
+				"Medlemmar"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(81);
+		table.setToolTipText("");
+		scrollPane.setViewportView(table);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(213, 199, 56, 16);
+		contentPane.add(lblNewLabel);
 	}
-
 }
